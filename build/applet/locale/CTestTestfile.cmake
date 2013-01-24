c54a3de3cd3551d0e1f2360ccd53b8e203381f09
Bart Kelsey <elbarto@gmail.com>
2013-01-24 14:08:20 -0500

    accidentally included build directory

--- build/applet/locale/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/adjustable-clock/plasmoid-adjustable-clock/applet/locale
-# Build directory: /home/user/adjustable-clock/plasmoid-adjustable-clock/build/applet/locale
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
